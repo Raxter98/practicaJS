42add943b6e2ea2091916f14e5bb52cbe0849c28
Marcos Valero <soymarcos354@gmail.com>
2022-12-09 16:33:23 -0300

    Primer commit prueba de Js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+variable = document.querySelector(".titulo");
+variable.style.backgroundColor = "red"
+
+window.sr = ScrollReveal();
+
+    sr.reveal(variable, {
+        duration:1000,
+        origin: "left",
+        distance: "600px"
+    })
+
